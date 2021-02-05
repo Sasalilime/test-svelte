2a5f72034c7e35f8ed42289f15d3a137d85757fa
salim <salimtison@gmail.Com>
2021-02-05 14:10:35 +0100

    first commit

---
+++ JS-local-storage-master/script.js
@@ -0,0 +1,30 @@
+const name = () => {
+  let sayHello = document.getElementById('sayHello');
+  let nameStorage = localStorage.getItem('nom');
+
+  if (nameStorage == null){
+    sayHello.innerHTML = "Bonjour à toi l'inconnu, merci de renseigner ton pseudo";
+  } else {
+    sayHello.innerHTML = `Bonjour ${nameStorage}`;
+  }
+};
+
+const visit = () => {
+  let visitDisplay = document.getElementById('visitDisplay');
+  let visitCount = localStorage.getItem('visites');
+
+  visitCount++;
+  localStorage.setItem('visites', visitCount);
+  visitDisplay.innerHTML = `Nombre de visites : ${visitCount}`;
+};
+
+function setData(){
+
+  let pseudo = document.getElementById('pseudo').value;
+  localStorage.setItem('nom', pseudo);
+};
+
+name();
+visit();
+
+document.cookie = "pseudo=Juju";
