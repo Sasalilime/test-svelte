6e094aa2384ca9c81be37664b141088c378d7770
salimtison <salimtison@gmail.com>
2021-02-18 11:32:46 +0100

    Svelte forms

--- src/App.svelte
+++ src/App.svelte
@@ -3,6 +3,8 @@
     import Toggle from "./Toggle.svelte";
     import Else from "./Else.svelte";
     import Each from "./Each.svelte";
+    import Input from "./Input.svelte";
+    import Form from "./Form.svelte";
 
     let titre = "Mes lignesssss";
 
@@ -27,19 +29,27 @@
     }
 
 </script>
+<main>
+    <div class="flex justify-around">
+        <List1 class="p-1" montitre={titre} {...myObject}/>
+        <div class="p-1"><p>{compteur}</p>
+            <button on:click={increment}>+1</button>
+            <button on:click={decrement}>-1</button>
+            <button on:click={reset}>Reset</button>
+        </div>
+        <Toggle class="p-1"/>
+        <Else class="p-1"/>
+        <Each class="p-1"/>
 
-<main class="flex justify-around">
-    <List1  class="p-1" montitre={titre} {...myObject}/>
-    <div class="p-1"><p>{compteur}</p>
-        <button on:click={increment}>+1</button>
-        <button on:click={decrement}>-1</button>
-        <button on:click={reset}>Reset</button>
     </div>
-    <Toggle class="p-1"/>
-    <Else class="p-1"/>
-    <Each class="p-1"/>
+    <div class="flex justify-around">
+        <Input/>
+        <Form/>
+    </div>
+
 
 </main>
+
 
 <style global>
     @import 'tailwindcss/base';
---
+++ src/Form.svelte
@@ -0,0 +1,35 @@
+<script>
+
+    let objDataForm = {
+        pays: '',
+        superficie: 0,
+        pop: 0
+    }
+
+    let pays;
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        pays = objDataForm.pays;
+
+        objDataForm.pays = '';
+    }
+
+</script>
+
+
+<main class="container">
+    <form on:submit={handleSubmit} class="">
+        <label for="pays">Votre pays</label>
+        <input bind:value={objDataForm.pays} id="pays" type="text">
+        <h1 class="border-b-2 mb-4">Votre pays est {objDataForm.pays}</h1>
+        <label for="population">La population</label>
+        <input bind:value={objDataForm.pop} id="population" type="number">
+        <h2 class="border-b-2 mb-4">La population est de {objDataForm.pop}</h2>
+        <label for="superficie">La supericie</label>
+        <input bind:value={objDataForm.superficie} id="superficie" type="number">
+        <h2 class="border-b-2 mb-4">La superficie est {objDataForm.superficie}</h2>
+        <button type="submit" class="2xl:bg-blue-300 rounded-2xl px-4 h-8">Envoyer les données</button>
+    </form>
+
+</main>
